6c109205a79fb073f4e364e2a582e782260e497c
unknown <savevsgames@gmail.com>
2023-09-01 21:54:47 -0600

    First Commit for Shopify Theme 01

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './layout/*.liquid',
+  './sections/*.liquid',
+  './snippets/*.liquid',
+  './templates/*.liquid',
+  './templates/customers/*.liquid',
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
