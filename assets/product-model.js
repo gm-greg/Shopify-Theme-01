0337901fa0e5c36e00fdaf32e53d036370d20b63
gm-greg <savevsgames@gmail.com>
2023-09-09 02:30:51 -0600

    the error was a capitolized P!

--- assets/product-model.js
+++ assets/product-model.js
@@ -1,31 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    try {
-        class ProductModel extends HTMLElement {
-            constructor() {
-                super();
-                this.openModelModal();
-            }
-
-            getMediaID() {
-                const id = this.getAttribute('data-media-id');
-                return id;
-            }
-
-            getModal() {
-
-            }
-
-            openModelModal() {
-                console.log("The element is working correctly.");
-                const mediaID = this.getMediaID();
-                const modal = this.getModal();
-                console.log(mediaID);
-            }
-        }
-
-        const ProductModel = customElements.define('product-model', ProductModel);
-
-    } catch(e) { 
-        console.log("Swiper JS is not defined"); 
+class ProductModel extends HTMLElement {
+    constructor() {
+        super();
+        this.openModelModal();
     }
-});
+
+    getMediaID() {
+        const id = this.getAttribute('data-media-id');
+        return id;
+    }
+
+    getModal() {
+
+    }
+
+    openModelModal() {
+        console.log("The element is working correctly.");
+        const mediaID = this.getMediaID();
+        const modal = this.getModal();
+        console.log(mediaID);
+    }
+}
+
+const productModel = customElements.define('product-model', ProductModel);
