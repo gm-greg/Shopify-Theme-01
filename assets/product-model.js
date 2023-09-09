cb1d8ae2f4613bbb8b88f931d62f491a6aa546f0
gm-greg <savevsgames@gmail.com>
2023-09-09 03:33:50 -0600

    reveal auto

--- assets/product-model.js
+++ assets/product-model.js
@@ -23,6 +23,8 @@ class ProductModel extends HTMLElement {
             modal.querySelector('#body').innerHTML = "";
             const template = document.querySelector(`product-model[data-media-id="${mediaID}"] > template`);
             const clone = template.content.cloneNode(true);
+            modal.querySelector('#body').appendChild(clone);
+            modal.querySelector('#body > model-viewer').setAttribute("reveal", "auto");
         })
     }
 }
