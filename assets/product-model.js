bd047762a4948715de3a6f1c89bd5d5245165620
gm-greg <savevsgames@gmail.com>
2023-09-09 01:34:34 -0600

    is script loading try catch

--- assets/product-model.js
+++ assets/product-model.js
@@ -1,24 +1,31 @@
-class ProductModel extends HTMLElement {
-    constructor() {
-        super();
-        this.openModelModal();
+document.addEventListener('DOMContentLoaded', function() {
+    try {
+        class ProductModel extends HTMLElement {
+            constructor() {
+                super();
+                this.openModelModal();
+            }
+
+            getMediaID() {
+                const id = this.getAttribute('data-media-id');
+                return id;
+            }
+
+            getModal() {
+
+            }
+
+            openModelModal() {
+                console.log("The element is working correctly.");
+                const mediaID = this.getMediaID();
+                const modal = this.getModal();
+                console.log(mediaID);
+            }
+        }
+
+        const ProductModel = customElements.define('product-model', ProductModel);
+
+    } catch(e) { 
+        console.log("Swiper JS is not defined"); 
     }
-
-    getMediaID() {
-        const id = this.getAttribute('data-media-id');
-        return id;
-    }
-
-    getModal() {
-
-    }
-
-    openModelModal() {
-        console.log("The element is working correctly.");
-        const mediaID = this.getMediaID();
-        const modal = this.getModal();
-        console.log(mediaID);
-    }
-}
-
-const ProductModel = customElements.define('product-model', ProductModel);
+});
